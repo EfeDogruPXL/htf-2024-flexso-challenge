7188e119151b35554e15228330fee8282a8c46a4
EfeDogruPXL <efe.dogru@student.pxl.be>
2024-11-19 15:42:31 +0100

    search

--- app/frontend/webapp/controller/Master.controller.ts
+++ app/frontend/webapp/controller/Master.controller.ts
@@ -4,6 +4,12 @@ import UIComponent from "sap/ui/core/UIComponent";
 import GenericTile from "sap/m/GenericTile";
 import { LayoutType } from "sap/f/library";
 import UI5Event from "sap/ui/base/Event";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import Filter from "sap/ui/model/Filter";
+import SearchField from "sap/m/SearchField";
+import MultiComboBox from "sap/m/MultiComboBox";
+import Panel from "sap/m/Panel";
+import ListBinding from "sap/ui/model/ListBinding";
 
 
 /**
@@ -24,7 +30,38 @@ export default class Master extends Controller {
     });
   }
 
-  private onSearch(event: UI5Event) {}
+  private onSearch(event: UI5Event) {
+    const oView = this.getView()!;
+    const oSearchField = oView.byId("id.SearchField") as SearchField;
 
-  private clearFilters(event: UI5Event) {}
+    const oList = this.byId("idHrmSystemsPane") as any;
+    const oBinding = oList.getBinding("content");
+
+    const aFilters: Filter[] = [];
+    const query = oSearchField.getValue();
+    if (query) {
+      aFilters.push(new Filter("name", FilterOperator.Contains, query));
+    }
+
+    oBinding.filter(aFilters); // Apply filter to the binding
+  }
+
+  public clearFilters(event: UI5Event): void {
+    const oView = this.getView()!;
+    const oSearchField = oView.byId("id.SearchField") as SearchField;
+    const oComboBox = oView.byId("id.AlienStatusComboBox") as MultiComboBox;
+    const oPanel = oView.byId("idHrmSystemsPane") as Panel;
+    const oBinding = oPanel.getBinding("content") as ListBinding;
+
+    // Clear search field
+    oSearchField.setValue("");
+
+    // Clear selected keys in the MultiComboBox
+    oComboBox.setSelectedKeys([]);
+
+    // Remove any applied filters
+    if (oBinding) {
+      oBinding.filter([], "Application");
+    }
+  }
 }
